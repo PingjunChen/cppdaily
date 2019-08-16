f18b2d992101c4e4c96b9eb12f2111cf534ab531
PingjunChen <chenpingjun@gmx.com>
2019-08-16 02:08:55 -0400

    add more tests for template in_range

--- test_daily.cc
+++ test_daily.cc
@@ -21,6 +21,8 @@ using namespace daily;
 
 DAILY_TEST_CASE(daily_test) {
     std::cout << ">> Testing Mathmatics \n" << std::flush;
+    DAILY_TEST_ASSERT(in_range(2, 0, 2) == true);
+    DAILY_TEST_ASSERT(in_range('a', 'b', 'z') == false);
     DAILY_TEST_ASSERT(in_range(1.0, 0.0, 2.0) == true);
     std::cout << "Congratulations, all tests are passed!!!\n" << std::flush; 
 }
